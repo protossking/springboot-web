925fedff4150134bebd3c707c774178cb717792f
upgrademarine5 <upgrademarine5@icloud.com>
2021-12-08 15:36:43 +0900

    add update

---
+++ src/main/java/com/yang/post/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.yang.post.web;
+
+import com.yang.post.domain.Posts;
+import com.yang.post.service.PostsService;
+import com.yang.post.web.dto.PostsResponseDto;
+import com.yang.post.web.dto.PostsSaveRequestDto;
+import com.yang.post.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
